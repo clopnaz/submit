3efb4425bcc059b2cf07d9cea6204dd33ccf94a6
U-PODS\jwalsh3 <jwalsh3@Ansys-19-19.pods.bu.int>
2019-02-05 18:23:44 -0500

    make test pass

--- submit.bat
+++ submit.bat
@@ -1,8 +1,15 @@
-@echo off
+rem @echo off
+@setlocal EnableDelayedExpansion
 set rev=192
 set inpfile=%~dpf1.dat
 set outfile=%~dpf1.txt
 set jobn=%~n1
-set ansexe=ansys%rev%
+set anspath=!ansys%rev%_dir!\bin\%ansys_sysdir%
+REM echo %anspath%
+set ansexe=%anspath%\ansys%rev%
 echo "calling %ansexe% -b -i %inpfile% -o %outfile% -j %jobn%"
-REM ansys%rev% -b -i 
+@echo on 
+"%ansexe%" -b -i %inpfile% -o %outfile% -j %jobn%
+@echo off
+endlocal
+exit /b
--- test/test_run.bat
+++ test/test_run.bat
@@ -1,17 +1,15 @@
-rem @echo off
+@echo off
 setlocal
 pushd "%~dp0"
 REM pwd
 echo "TEST BEGINS"
-echo /com,==================== thetest >test.dat
-echo /com,test passed! thetest >>test.dat
-echo /com,==================== thetest >>test.dat
+echo /com,thetest ==================== >aatest.dat
+echo /com,thetest test passed! >>aatest.dat
+echo /com,thetest ==================== >>aatest.dat
 call ..\submit.bat aatest
 echo "TEST RESULT" 
-@echo on
-findstr thetest test.txt 
+findstr /b .thetest aatest.txt 
 del aatest* 
-rem del test.txt
 echo "TEST ENDS"
 popd
 endlocal
