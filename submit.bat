e97a551f73f8a14e4894888117c401380b69a2a4
U-PODS\jwalsh3 <jwalsh3@Ansys-19-19.pods.bu.int>
2019-02-05 16:36:44 -0500

    .bat and .txt vars

--- submit.bat
+++ submit.bat
@@ -1,6 +1,9 @@
 @echo off
 set rev=192
+set inpfile=%~dpf1.dat
+set outfile=%~dpf1.txt
+set ansexe=ansys%rev%
 echo "we will call ansys%rev%"
-echo "input file %~dpnf1.dat"
-echo "output file %~dpnf1.txt"
+echo "input file %inpfile%"
+echo "output file %outfile%"
 REM ansys%rev% -b -i 
