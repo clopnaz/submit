f1a6b8217f6e5c077b6ad2634bc3fa69af6c7d5c
clopnaz <clopnaz@gmail.com>
2019-03-04 09:21:30 -0500

    add script for running gui

---
+++ gui.bat
@@ -0,0 +1,12 @@
+@echo off
+@setlocal EnableDelayedExpansion
+set rev=192
+set jobn=%~n1
+set anspath=!ansys%rev%_dir!\bin\%ansys_sysdir%
+REM echo %anspath%
+set ansexe=%anspath%\ansys%rev%
+@echo on 
+"%ansexe%" -smp -np 4  -j %jobn% -g
+@echo off
+endlocal
+exit /b
