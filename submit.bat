be04837a6c78c154e86ad41a79e54d0ca41bbf97
clopnaz <clopnaz@gmail.com>
2019-03-04 09:21:11 -0500

    set -smp

--- submit.bat
+++ submit.bat
@@ -8,7 +8,7 @@ set anspath=!ansys%rev%_dir!\bin\%ansys_sysdir%
 REM echo %anspath%
 set ansexe=%anspath%\ansys%rev%
 @echo on 
-"%ansexe%" -dis -np 4 -b -i %inpfile% -o %outfile% -j %jobn%
+"%ansexe%" -smp -np 4 -b -i %inpfile% -o %outfile% -j %jobn%
 @echo off
 endlocal
 exit /b
